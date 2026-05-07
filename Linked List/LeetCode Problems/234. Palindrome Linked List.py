7bc6fd5722ee26510791f8fb6dec81e78a930562
IhorKastranetsUCU <kastranets.pn@ucu.edu.ua>
2026-05-07 22:30:43 +0300

    Solved Palindrome Linked List problem on LeetCode

---
+++ Linked List/LeetCode Problems/234. Palindrome Linked List.py
@@ -0,0 +1,21 @@
+from collections import deque
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        stack = deque()
+        cur = head
+        while cur:
+            stack.append(cur.val)
+            cur = cur.next
+        cur = head
+        while cur:
+            item = stack.pop()
+            if item != cur.val:
+                return False
+            cur = cur.next
+        return True
